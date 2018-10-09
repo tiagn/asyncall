a4c972824cbfa116904746448e9e44e82b28ba54
yechujiong <347618578@qq.com>
2018-10-09 19:52:33 +0800

    add LICENSE and setup

---
+++ setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+NAME = 'asyncall'
+DESCRIPTION = 'simpler and more convenient way of asynchronous invocation'
+URL = 'https://github.com/tiagn/asyncall'
+EMAIL = '347618578@qq.com'
+AUTHOR = 'tiagn'
+REQUIRES_PYTHON = '>=2.7'
+VERSION = '1.0.0'
+
+REQUIRED = [
+]
+
+EXTRAS = {
+}
+
+# The rest you shouldn't have to touch too much :)
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
